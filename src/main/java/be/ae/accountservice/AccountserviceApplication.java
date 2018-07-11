44b7f9c6aec23e0cd4dad18f7490d80349312d58
sstoelen <sebastian.stoelen@gmail.com>
2018-07-11 10:49:47 +0200

    ✨✨ First commit ✨✨

---
+++ src/main/java/be/ae/accountservice/AccountserviceApplication.java
@@ -0,0 +1,12 @@
+package be.ae.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountserviceApplication.class, args);
+	}
+}
